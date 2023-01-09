54ac09811b42cb0ed17c321b5dea74b6b9ed7bc1
Gabriele Cossetti <78553671+Gabriele-Cossetti@users.noreply.github.com>
2023-01-09 18:02:58 +0100

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(112, 34)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(231, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(112, 34)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(449, 12)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(112, 34)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Azzera"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 14.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(449, 546)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(105, 38)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Label1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(449, 521)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(94, 25)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "1 sec /100"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 14.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(309, 546)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(105, 38)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Label2"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(319, 521)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 25)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Sec"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 14.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label5.Location = New System.Drawing.Point(160, 546)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(105, 38)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Label5"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI", 14.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label6.Location = New System.Drawing.Point(19, 546)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(105, 38)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "Label6"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(30, 521)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(41, 25)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "Ore"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(171, 521)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(62, 25)
+        Me.Label8.TabIndex = 11
+        Me.Label8.Text = "Minuti"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Info
+        Me.BackgroundImage = Global.Cronometro_03.My.Resources.Resources.Quadrante_Orologio_20
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ClientSize = New System.Drawing.Size(573, 609)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.Text = "Cronometro"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,106 @@
+﻿'Tratto da Manuale Farri-Piotti-Sbroggiò pag. 704 Esercizio 100
+Imports System.Configuration
+Imports System.Threading
+Public Class Form1
+    Dim ImmagineOrologio As Bitmap = My.Resources.Quadrante_Orologio_20
+    Dim Ore As Integer
+    Dim Minuti As Integer
+    Dim Secondi As Integer
+    Dim Centesimi As Integer
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.DoubleBuffered = True
+        Timer1.Enabled = False
+        Timer1.Interval = 10     'in millisecondi con 1000 = 1 secondo, 100 decimi di secondo, 10 centesimi di secondo
+        Ore = 0
+        Minuti = 0
+        Secondi = 0
+        Centesimi = 0
+        Label1.Text = Secondi
+        Label2.Text = Centesimi
+        Label5.Text = Minuti
+        Label6.Text = Ore
+        If Timer1.Enabled = False Then
+            Button3.Enabled = False
+            Button2.Enabled = False
+            Button1.Enabled = True
+        End If
+    End Sub
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Centesimi += 1
+        If Centesimi = "100" Then
+            Centesimi = 0
+            Secondi += 1
+        End If
+        If Secondi = "60" Then
+            Secondi = 0
+            Minuti += 1
+        End If
+        If Minuti = "60" Then
+            Minuti = 0
+            Ore += 1
+        End If
+        If Ore = "24" Then
+            Ore = 0
+        End If
+        Me.Invalidate()
+        Label1.Text = Centesimi
+        Label2.Text = Secondi
+        Label5.Text = Minuti
+        Label6.Text = Ore
+    End Sub
+    Private Sub Form1_Paint(sender As Object, e As System.Windows.Forms.PaintEventArgs) Handles Me.Paint
+        e.Graphics.SmoothingMode = Drawing2D.SmoothingMode.AntiAlias
+        Dim Tempo As Integer
+        Dim PuntoCentrale As New Point(Me.ClientRectangle.Width / 2, Me.ClientRectangle.Height / 2)
+        Tempo = Secondi
+        Secondi = (Tempo * 6) + 270
+        Dim ColoreSfondo As Color = Color.White
+        Dim PennaSecondi As New Pen(Color.Red, 1)
+        e.Graphics.DrawPie(PennaSecondi, PuntoCentrale.X - 130, PuntoCentrale.Y - 130, 260, 260, Secondi, 360)
+        Dim CancellaSecondi As New Pen(ColoreSfondo, 3)
+        e.Graphics.DrawEllipse(CancellaSecondi, PuntoCentrale.X - 130, PuntoCentrale.Y - 130, 260, 260)
+        e.Graphics.FillEllipse(Brushes.Black, PuntoCentrale.X - 10, PuntoCentrale.Y - 10, 20, 20)
+        e.Graphics.FillEllipse(Brushes.Red, PuntoCentrale.X - 4, PuntoCentrale.Y - 4, 8, 8)
+        Secondi = Tempo
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Enabled = True
+        Timer1.Start()
+        If Timer1.Enabled = True Then
+            Button1.Enabled = False
+            Button2.Enabled = True
+            Button3.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Enabled = False
+        Timer1.Stop()
+        If Timer1.Enabled = False Then
+            Button3.Enabled = True
+            Button2.Enabled = False
+            Button1.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Timer1.Enabled = False
+        Timer1.Stop()
+        Ore = 0
+        Minuti = 0
+        Secondi = 0
+        Centesimi = 0
+        Label1.Text = Secondi
+        Label2.Text = Centesimi
+        Label5.Text = Minuti
+        Label6.Text = Ore
+        Me.Invalidate()
+        If Timer1.Enabled = False Then
+            Button3.Enabled = False
+            Button2.Enabled = False
+            Button1.Enabled = True
+        End If
+    End Sub
+End Class
+
